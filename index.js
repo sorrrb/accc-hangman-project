0a2fbaacb25a323fc8c86a4f97f780ccd70c7654
Christopher Druke <cdrukepike@gmail.com>
2024-04-05 16:04:15 -0400

    Implement formatted console symbols

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import prompt from "readline-sync";
 import wordBank from './word-bank.js';
+
+const CONSOLE_SYMBOLS = [
+  '______\n|/ |\n| \n| \n| \n| \n|_____\n',
+  '______\n|/ |\n| (_)\n| \n| \n| \n|_____\n',
+  '______\n|/ |\n| (_)\n|  |\n|  |\n| \n|_____\n',
+  '______\n|/ |\n| (_)\n| \\|\n|  |\n| \n|_____\n',
+  '______\n|/ |\n| (_)\n| \\|/\n|  |\n| \n|_____\n',
+  '______\n|/ |\n| (_)\n| \\|/\n|  |\n| /\n|_____\n',
+  '______\n|/ |\n| (_)\n| \\|/\n|  |\n| / \\\n|_____\n'
+];
